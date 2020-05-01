d59eb5fbc2fa39b3e51fd4b4424e01dd51ad4e68
Mondal Bidisha <mondal.bidisha@talview.com>
2020-05-02 03:32:36 +0530

    returning weather info basis location entered

--- weather-check/server.js
+++ weather-check/server.js
@@ -3,44 +3,44 @@ const BodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
-const requestURL = 'http://api.weatherstack.com/current';
-const access_token = '881e74e04132d381cc2fc0b3c9429191';
+const requestURL = 'http://api.weatherstack.com/current'; // Weather API used to get weather information
+const access_token = '881e74e04132d381cc2fc0b3c9429191'; // access_token, allows 1000 API calls/month
 
 app.set('view engine', 'ejs'); // set the ejs as the rendering engine. ejs can be used out of the box from the 'views' folder
 app.use(express.static('public')); // Express wont allow access to this file by default, so we need to expose it. This code allows us to access all of the static files within the ‘public’ folder.
 app.use(BodyParser.urlencoded({ extended: true })); 
 
 app.get('/', function(request, response) {
-  response.render('index', {weather: null, error: null});
+  response.render('index');
 })
 
 app.post('/', function (req, response) {
-  response.render('index');
   console.log(req.body.city);
   let city = req.body.city;
-  let requestAPI = `${requestURL}?access_key=${access_token}&query=${city}`; 
-  // request(requestAPI, function (err, res, body) {
-  //   if(err){
-  //     response.render('index', {weather: null, error: 'Error, please try again'});
-  //   } else {
-  //     let weather = JSON.parse(body)
-  //     if(weather.main == undefined){
-  //       response.render('index', {weather: null, error: 'Error, please try again'});
-  //     } else {
-  //       let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-  //       response.render('index', {weather: weatherText, error: null});
-  //     }
-  //   }
-  // });
-  // // request(requestAPI, function(error, request, response) {
-  // //   if(true) {
-  // //     response.render('index', {
-  // //       weather: null, 
-  // //       error: 'Error, please try again'
-  // //     })
-  // //   }
-  // // })
+  getWeatherInfo(city, (err, res) => {
+    if(err) {
+      return response.render('index', {error: "LOCATION NOT FOUND"}); 
+    }
+    response.render('index', {error: res.current.weather_descriptions[0]});
+    console.log(res.current.weather_descriptions[0]);
+  })
 })
+                
+const getWeatherInfo = (city, callback) => {
+  let requestAPI = `${requestURL}?access_key=${access_token}&query=${city}`; 
+  let parsedBodyData;
+  request(requestAPI, function (error, response, body) {
+    if(error) {
+      console.log("ERROR! SOMETHING WENT WRONG.");      
+    } else if(response && body) {
+      parsedBodyData = JSON.parse(body);
+      if(parsedBodyData.success == false && parsedBodyData.error) {
+        console.log(parsedBodyData.error.info);      
+      }
+      callback(error, parsedBodyData);
+    }
+  });
+}
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
